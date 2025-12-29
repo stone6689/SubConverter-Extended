f47d8d516b8dafd52c01eb5f3355d4cff8a640fe
Aethersailor <22260104+Aethersailor@users.noreply.github.com>
2025-12-29 21:04:59 +0800

    chore: bump version to 1.0.5 and optimize Docker workflow
    - Update version from 1.0.3 to 1.0.5
    - Add dev branch support to Docker build workflow
    - Configure branch-specific tags (master->latest, dev->dev)

--- src/version.h
+++ src/version.h
@@ -1,7 +1,7 @@
 #ifndef VERSION_H_INCLUDED
 #define VERSION_H_INCLUDED
 
-#define VERSION "1.0.3"
+#define VERSION "1.0.5"
 #define BUILD_ID "" // Will be replaced during Docker build
 
 #endif // VERSION_H_INCLUDED
